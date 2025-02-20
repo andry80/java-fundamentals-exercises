2683d495fb291b35b79c3c2dd758c4b64c2ddbcd
andry80 <8888appl8888@gmail.com>
2025-02-20 20:25:13 +0200

    lesson14

--- 4-0-object-oriented-programming/4-3-1-flight-search/src/main/java/com/bobocode/oop/service/FlightService.java
+++ 4-0-object-oriented-programming/4-3-1-flight-search/src/main/java/com/bobocode/oop/service/FlightService.java
@@ -1,8 +1,12 @@
 package com.bobocode.oop.service;
 
+import com.bobocode.oop.data.FlightDao;
 import com.bobocode.util.ExerciseNotCompletedException;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * {@link FlightService} provides an API that allows to manage flight numbers
@@ -12,6 +16,7 @@ import java.util.List;
  */
 public class FlightService {
 
+    FlightDao flightDao = new FlightDao();
     /**
      * Adds a new flight number
      *
@@ -19,7 +24,7 @@ public class FlightService {
      * @return {@code true} if a flight number was added, {@code false} otherwise
      */
     public boolean registerFlight(String flightNumber) {
-        throw new ExerciseNotCompletedException();
+        return flightDao.register(flightNumber);
     }
 
     /**
@@ -29,6 +34,13 @@ public class FlightService {
      * @return a list of found flight numbers
      */
     public List<String> searchFlights(String query) {
-        throw new ExerciseNotCompletedException();
+        Set<String> st = flightDao.findAll();
+        List<String> lst = new ArrayList<String>();
+        for (String s :st) {
+            if (s.contains(query)) {
+                lst.add(s);
+            }
+        }
+        return lst;
     }
 }
